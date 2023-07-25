99ab920bab619b0fc9f38f6f632faca444e8e6cc
Arthur Martins Freire <mfarthurr@gmail.com>
2023-07-25 20:08:28 -0300

    Initial Commit

---
+++ SleepData/SleepDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepDataApp.swift
+//  SleepData
+//
+//  Created by Arthur Martins on 25/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SleepDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
